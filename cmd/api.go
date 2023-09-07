56a0ea019668f0ee1971a598e955f757c86b386e
ant <ant@ant>
2023-09-07 11:39:25 +0300

    .gitlab-ci deploy

--- cmd/api.go
+++ cmd/api.go
@@ -24,6 +24,7 @@ func runApi() {
 	e := echo.New()
 
 	users := diContainer.Get("api.users").(*api.UsersAction)
+	e.GET("/gandon", users.GetAll)
 	e.GET("/users", users.GetAll)
 	e.GET("/users/:id", users.GetById)
 	e.POST("/users", users.Create)
